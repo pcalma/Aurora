ab860131fde5f0e8e83172e6b98e9bd773898e59
pcalma <paupaualmario92@gmail.com>
2024-08-29 10:01:53 -0500

    subio correctamente tablas y mapeo a BD

--- src/main/java/com/moncho/ecommerce/Aurora1Application.java
+++ src/main/java/com/moncho/ecommerce/Aurora1Application.java
@@ -2,10 +2,11 @@ package com.moncho.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 //dejar si no ahi BD 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class Aurora1Application {
 
 	public static void main(String[] args) {
